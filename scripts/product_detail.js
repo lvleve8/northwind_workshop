2791fae5f7f3335a9b53a386905d73e64d54f554
yvette rodeo <yrodeo@yearup.org>
2024-06-10 14:20:22 -0500

    displayed product search

--- scripts/product_detail.js
+++ scripts/product_detail.js
@@ -1,29 +1,117 @@
 
 "use strict"
- 
-window.onload = () =>{
- 
-    //console.log("Hello")
- 
+
+window.onload = () => {
+
     let dropdown = document.querySelector("#productSearchDDL");
 
+    applyToDropdown();
+
+    
     dropdown.addEventListener("change", displayAll);
- 
 }
-async function displayAll(){
-    let dropdown = document.querySelector("#productSearchDDL");
-    let tbody = document.querySelector("#tableBody")
+
+async function displayAll() {
+    let dropdown = document.querySelector("#productDetailDDL");
+
+    let tbody = document.querySelector("#tableBody");
+
     let productsList = await getAllData();
-    
-    if(dropdown.value === "all"){
-    
-    productsList.forEach((product) => {
-    
-         buildTable(tbody, product)
-    });
-    
-    
+
+    if (dropdown.value === "all") {
+
+       
+        productsList.forEach((product) => {
+
+          
+            buildTable(tbody, product)
+        });
+
+
+    } else if (dropdown.value === "category") {
+
+        
+
     }else{
-        console.log("Error")
+        console.log("error");
     }
-    
+
+
+}
+
+function buildTable(tbody, data) {
+
+ 
+    let row = tbody.insertRow();
+
+    let productNCell = row.insertCell();
+    productNCell.innerHTML = data.productName;
+
+    let priceCell = row.insertCell();
+    priceCell.innerHTML = data.unitPrice;
+
+    let inStockCell = row.insertCell();
+    inStockCell.innerHTML = data.unitsInStock;
+
+    let suppCell = row.insertCell();
+    suppCell.innerHTML = data.supplier;
+
+
+
+
+}
+
+async function getAllData() {
+
+    try {
+        let response = await fetch("http://localhost:8081/api/products");
+        let list = await response.json();
+
+        return list;
+    } catch (err) {
+        console.log("error")
+        throw new Error(err)
+    }
+
+}
+
+
+async function getCategories(){
+    try {
+        let response = await fetch("http://localhost:8081/api/categories");
+        let cat = await response.json();
+
+        return cat;
+    } catch (err) {
+        console.log("error")
+        throw new Error(err)
+    }
+}
+
+async function applyToDropdown(){
+
+    let catDrop = document.querySelector("#categorySearch");
+
+    let allCategories = await getCategories();
+
+    let defaultOption = document.createElement("option");
+    defaultOption.value = "0";
+    defaultOption.innerText = "----Select Category----";
+ 
+    catDrop.appendChild(defaultOption);
+
+    allCategories.forEach((category) => {
+
+       
+        let newOption = document.createElement("option");
+
+        newOption.value = 0;
+
+        newOption.textContent = category.name;
+
+        catDrop.appendChild(newOption);
+
+
+    })
+
+}
--- scripts/product_search.js
+++ scripts/product_search.js
@@ -1,9 +1,77 @@
+"use strict";
 
-"use strict"
- 
-window.onload = () =>{
- 
-    console.log("ITS ALIVE!")
- 
- 
+window.onload = () => {
+    console.log("hello )");
+
+    let productSearchDDL = document.querySelector("#productSearchDDL");
+    productSearchDDL.addEventListener("change", checkOptions);
+
+};
+
+
+function hideTable() {
+    let tableOverall = document.querySelector("#allProductTable");
+    tableOverall.style.display = "none";
+}
+
+function showTable(){
+    let tableOverall = document.querySelector("#allProductTable");
+    console.log(tableOverall);
+    tableOverall.style.display = "table";
+}
+
+function checkOptions() {
+    let value = document.querySelector("#productSearchDDL").value;
+
+    if (value === "viewAll") {
+        getViewAllData()
+        showTable()
+        console.log("you chose the viewAll option"); 
+    } else if (value === "category"){
+     
+        console.log("you chose the category option")
+    } else {
+        if(value === ""){
+            hideTable()
+            console.log("you chose the Select one option");
+        }
+    }
+}
+
+
+async function getViewAllData() {
+    try {
+        let response = await fetch('http://localhost:8081/api/products/');
+        if (!response.ok) {
+            throw new Error("Failed to fetch allProductsData");
+        }
+        let data = await response.json();
+      
+        makeViewAllDataTable(data); 
+    } catch (error) {
+        console.error(`Error products: ${error.message}`);
+    }
+}
+
+async function makeViewAllDataTable(products) {
+    try {
+        let tbodyTable = document.querySelector("#tbodyTable");
+     
+        tbodyTable.innerHTML = "";
+
+        products.forEach(product => {
+            let row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${product.productName}</td>
+                <td>${product.unitPrice}</td>
+                <td>${product.unitsInStock}</td>
+                <td>${product.categoryId}</td>
+                <td>${product.supplier}</td>
+                <td>${product.discounted ? 'Yes' : 'No'}</td>
+            `;
+            tbodyTable.appendChild(row);
+        });
+    } catch (error) {
+        console.error(`Error making data table: ${error.message}`);
+    }
 }
